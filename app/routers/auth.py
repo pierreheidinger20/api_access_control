1c400fee80243d0317301739f0826ab4598f5443
PIERRE HEIDINGER <150283183+pboronda@users.noreply.github.com>
2025-08-19 19:51:22 -0300

    services

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.services.auth import login_user
 from app.schemas.users import UserLogin
+from sqlalchemy.orm import Session
+from app.db import db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+def get_db():
+    db_session = db.SessionLocal()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
 
 @router.post("/login")
-def login(user: UserLogin):
-    return login_user(user)
+def login(user: UserLogin,db: Session = Depends(get_db)):
+    return login_user(user,db)
 
--- app/services/auth.py
+++ app/services/auth.py
@@ -3,21 +3,14 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.users import UserLogin
 from app.utils.security import create_access_token,verify_password
-from fastapi import Depends, HTTPException
+from fastapi import HTTPException
 from app.db import db
 from passlib.context import CryptContext
 from app.utils.security import create_access_token
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_db():
-    db_session = db.SessionLocal()
-    try:
-        yield db_session
-    finally:
-        db_session.close()
-
-def login_user(user: UserLogin, db: Session = Depends(get_db)):
+def login_user(user: UserLogin,db: Session):
     db_user = db.query(User).filter(User.username == user.username).first()
     if not db_user or not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectos")
