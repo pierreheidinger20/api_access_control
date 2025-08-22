e8b8795dcd08b28cb94ea931de8ad533289d97e3
PIERRE HEIDINGER <150283183+pboronda@users.noreply.github.com>
2025-08-22 12:19:23 -0300

    change payload token

--- app/services/auth.py
+++ app/services/auth.py
@@ -16,19 +16,19 @@ def login_user(user: UserLogin,db: Session):
     db_user = db.query(User).filter(User.username == user.username).first()
     if not db_user or not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectos")
-    token = create_access_token({"sub": db_user.username})
-    
-    return create_token_response(token,db_user)
-
-def create_token_response(token: str,user: User) -> TokenResponse:
-    userOut  = UserOut(
-        username=user.username,
-        email=user.email,
-        full_name=user.full_name
+    user_out  = UserOut(
+        username=db_user.username,
+        email=db_user.email,
+        full_name=db_user.full_name
     )
+    token = create_access_token(user_out.model_dump())
+    
+    return create_token_response(token,user_out)
+
+def create_token_response(token: str,user_out: UserOut) -> TokenResponse:
     return TokenResponse(
         access_token=token,
         token_type=settings.token_type,
         expires_in= settings.access_token_expire_minutes * 60,  # Convert minutes to seconds
-        user=userOut
+        user=user_out
     )
